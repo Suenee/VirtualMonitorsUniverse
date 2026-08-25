f4c82162502665780bb6aff0a8f4d8c5c7ca52ce
Sueneé <info@easyweby.cz>
2026-08-25 09:13:42 +0200

    Add first automated Core test

---
+++ tests/Core.Tests/VirtualMonitorInfoTests.cs
@@ -0,0 +1,26 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VirtualMonitorsUniverse.Core.Tests;
+
+[TestClass]
+public sealed class VirtualMonitorInfoTests
+{
+    [TestMethod]
+    public void RecordPreservesDisplayGeometry()
+    {
+        var monitor = new VirtualMonitorInfo(
+            "test-monitor",
+            @"\\.\DISPLAY99",
+            @"ROOT\DISPLAY\0000",
+            true,
+            1920,
+            1080,
+            64,
+            -1080);
+
+        Assert.AreEqual(1920, monitor.Width);
+        Assert.AreEqual(1080, monitor.Height);
+        Assert.AreEqual(64, monitor.X);
+        Assert.AreEqual(-1080, monitor.Y);
+    }
+}
